28a7ceab00ab0128d2a81de1cac983ab5c8e6ff7
chongchen20 <chenchong446337@gmail.com>
2020-08-04 16:50:04 -0400

    updated from Rstudio, 08042020

--- PN_behaviro_analysis.R
+++ PN_behaviro_analysis.R
@@ -4,12 +4,12 @@
 ## last updated 03242020
 
 cc_anti_behavior <- function(path_ID, frame_rate, length_pix) {
-  dat_anti <- read.csv(path_ID, header = F, stringsAsFactors = F)
-  
-  dat_anti <- dat_anti[-c(1:3), ]
-  ## only slect the position of head
-  dat_anti <- as.data.frame(apply(dat_anti, 2, as.numeric))
-  
+  dat_anti <- read.csv(path_ID, header = F, stringsAsFactors = F) %>% 
+    .[-c(1:3), ] %>% 
+    apply(., 2, as.numeric) %>%   ## only select the position of head
+    as.data.frame()
+
+
   ## normalize the data, zero point value from fiji
   dat_anti$V2<- dat_anti$V2 - dat_anti$V8
   dat_anti$V3<- dat_anti$V3 - dat_anti$V9
@@ -21,8 +21,8 @@ cc_anti_behavior <- function(path_ID, frame_rate, length_pix) {
   colnames(dat_anti)<- c("Frame", "Head_x","Head_y", "Prob", "Tail_x", "Tail_y")
   
   ## remove the frame for the first few frame
-  dat_anti <- dat_anti[seq(1, nrow(dat_anti), frame_rate/10), ]
-  dat_anti <- subset(dat_anti, Prob >0.5 | Frame> 60)
+  dat_anti <- dat_anti[seq(1, nrow(dat_anti), frame_rate/10), ] %>% 
+    subset(., Prob >0.5 | Frame> 60)
   
   ## reduce the frame rate to 5hz to mimimize the varaition 
   ##n<- 2
@@ -40,7 +40,7 @@ cc_anti_behavior <- function(path_ID, frame_rate, length_pix) {
   dat_anti$Tail_x <- abs(dat_anti$Tail_x * r_pix_mm)
   dat_anti$Tail_y <- abs(dat_anti$Tail_y * r_pix_mm)
   
-  chamber_div <- 168 ## the lenght of two chambels and the gap between
+  chamber_div <- 165.5 ## the lenght of two chambels and the gap between
   
   ## assign each frame in hot as 1 (48) and nor plate(30) as 2
   dat_anti$chamber <- ifelse(dat_anti$Head_y < chamber_div, "Hot","Nor")
@@ -80,18 +80,29 @@ cc_anti_behavior <- function(path_ID, frame_rate, length_pix) {
   dis_frame <- sqrt(x_diff^2 + y_diff^2)
   move_speed <- dis_frame*10
   
-  # total distance
+  move_speed_t <- zoo(move_speed) %>% 
+    rollapply(., width = 10, by = 10, FUN = mean, na.rm = TRUE, align = "left") %>% 
+    as.vector()
+  peak_threshold <- mean(move_speed_t) +  sd(move_speed_t)
+  peak_speed <- findpeaks(move_speed_t, nups = 2,minpeakdistance = 2, minpeakheight = peak_threshold) %>% 
+    .[,2]*10 %>% 
+    sort()
+  latency_accerlation <- (peak_speed[which(peak_speed> cross_hot_nor[1])][1] - cross_hot_nor[1])/10
+  # total distance, normalization
   total_distance <- round(sum(dis_frame), digits = 2)
   
   # speed in different chambers
   speed_hot <- mean(move_speed[which(dat_anti$chamber == "Hot")])
   speed_nor <- mean(move_speed[which(dat_anti$chamber == "Nor")])
-  
+  speed_compare <- speed_nor/speed_hot
   # average speed (3s) before crossing the chambers
   speed_to_nor_1st <- ifelse(cross_hot_nor[1]< 20, mean(move_speed[0:cross_hot_nor[1]]) ,mean(move_speed[(cross_hot_nor[1]-20):cross_hot_nor[1]]))
   speed_to_hot_1st <- ifelse(length(cross_nor_hot)==0, NA, mean(move_speed[(cross_nor_hot[1]-20):cross_nor_hot[1]]))
   
-  return(list(ratio_time, cross_latency_1st, cross_latency_2nd, total_distance, speed_to_nor_1st, speed_to_hot_1st, total_cross))
+  dat_anti_result <- tibble(ratio=ratio_time, latency1= cross_latency_1st, latency2 = cross_latency_2nd,
+                            total_distance = total_distance, speed1 = speed_to_nor_1st, speed2= speed_to_hot_1st, total_cross= total_cross,
+                            speed_compare=speed_compare, latency_accerlation= latency_accerlation)
+  return(dat_anti_result)
 }
 
 ## for opto anti behavior------
@@ -587,3 +598,362 @@ p_anti_prop <- ggplot(dat_anti_prop, aes(Group, Value, colour=Group))+
         panel.background = element_blank(),
         axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
   theme(legend.position = 'none')
+
+
+## for WT ctrl to prove the placebo analgesia-----
+dat_anti_ctrl <- vector(mode = "list", 7)
+dat_anti_con <- vector(mode = "list", 7)
+length_pix <- c(363,362,363,364,362,364,365)
+day_group <- c("Rm","Pre", "Pre", "Rm","Cond.","Cond.", "Test")
+for(i in c(1:7)){
+  path_ctrl <- str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_ctrl/T",i)
+  path_con <-  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_con/T",i)
+  dat_anti_ctrl[[i]]<- list.files(path_ctrl,pattern = ".csv", full.names = T ) %>% 
+    as.list() %>% 
+    mapply(cc_anti_behavior, ., frame_rate = 10, length_pix=length_pix[i], SIMPLIFY = F) %>% 
+    do.call(rbind,.) %>% 
+    add_column(Day = day_group[i], Group="Ctrl", ID=str_c("m",1:9), .before = 'ratio')
+  
+  dat_anti_con[[i]]<- list.files(path_con,pattern = ".csv", full.names = T ) %>% 
+    as.list() %>% 
+    mapply(cc_anti_behavior, ., frame_rate = 10, length_pix=length_pix[i], SIMPLIFY = F) %>% 
+    do.call(rbind,.) %>% 
+    add_column(Day = day_group[i], Group="Con", ID=str_c("m",1:10), .before = 'ratio')
+}
+
+dat_anti_wt <- do.call(rbind, dat_anti_ctrl) %>% 
+  rbind(., do.call(rbind, dat_anti_con)) %>% 
+  pivot_longer(-c(Day, Group, ID), names_to = "variable", values_to = "value" , values_drop_na = TRUE) %>% 
+  ddply(., .(ID, variable, Day, Group), summarise, 'value'= mean(value)) %>% 
+  filter(Day!="Rm") %>% 
+  mutate(Day=factor(Day, levels = c("Pre", "Cond.", "Test")), Group=factor(Group, levels = c("Ctrl", "Con")))
+
+dat_anti_wt_sta <- ddply(dat_anti_wt,.(variable,Day, Group), summarise, 'value'= mean(value))
+
+p_ratio <- dat_anti_wt %>% 
+  filter(variable=="ratio" & Day!="Cond.") %>% 
+  mutate(value=value*100) %>% 
+  ggplot(., aes(Day, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Time spend in 2nd plate (%)")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  scale_y_continuous(limits = c(0, 100), expand = c(0, 0))+
+  theme(legend.position = "none")+
+  annotate(x=c(1.8,1.8,2.2,2.2), y=c(85,87,87,85)+5,"path")+
+  annotate("text",x=2,y=92, label="***", size=5)
+
+p_ratio_test <- dat_anti_wt %>% 
+  filter(variable=="ratio" & Day=="Test") %>% 
+  wilcox.test(value~Group, .)
+
+p_latency1 <- dat_anti_wt %>% 
+  filter(variable=="latency1"& Day!="Cond.") %>% 
+  ggplot(., aes(Day, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency (s)")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+ scale_y_continuous(limits = c(0, 150), expand = c(0, 0))+
+  theme(legend.position = "none")+
+  annotate(x=c(1.8,1.8,2.2,2.2), y=c(100,102,102,100),"path")+
+  annotate("text",x=2,y=102, label="*", size=5)
+
+p_latency1_test <- dat_anti_wt %>% 
+  filter(variable=="latency1"& Day=="Test") %>% 
+  wilcox.test(value~Group, .)
+
+p_latency2 <- dat_anti_wt %>% 
+  filter(variable=="latency2"& Day!="Cond.") %>% 
+  ggplot(., aes(Day, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency (s)")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  scale_y_continuous(limits = c(5, 80))+
+  theme(legend.position = "none")+
+  annotate(x=c(1.8,1.8,2.2,2.2), y=c(70,72,72,70),"path")+
+  annotate("text",x=2,y=72, label="**", size=5)
+
+p_latency2_test <- dat_anti_wt %>% 
+  filter(variable=="latency2"& Day=="Test") %>% 
+  wilcox.test(value~Group,.)
+
+p_speed_compare <- dat_anti_wt %>% 
+  filter(variable=="speed_compare"& Day!="Cond.") %>% 
+  ggplot(., aes(Day, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Ratio of moving speed")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  theme(legend.position = "none")+
+  annotate(x=c(1.8,1.8,2.2,2.2), y=c(2,2.02,2.02,2),"path")+
+  annotate("text",x=2,y=2.02, label="***", size=5)
+
+p_speed_compare_test <- dat_anti_wt %>% 
+  filter(variable=="speed_compare"& Day=="Test") %>% 
+  wilcox.test(value~Group,.)
+
+p_total_cross <- dat_anti_wt %>% 
+  filter(variable=="total_cross"& Day!="Cond.") %>% 
+  ggplot(., aes(Day, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Ratio of moving speed")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  theme(legend.position = "none")+
+  annotate(x=c(1.8,1.8,2.2,2.2), y=c(2,2.02,2.02,2),"path")+
+  annotate("text",x=2,y=2.02, label="***", size=5)
+
+p_total_cross_test <- dat_anti_wt %>% 
+  filter(variable=="total_cross"& Day=="Test") %>% 
+  wilcox.test(value~Group,.)
+
+p_pain_compare1 <- plot_grid(p_ratio, p_latency1, p_latency2, p_speed_compare, ncol = 2)
+
+
+## for pain behavir, like licking, and wearing-----
+# mannualy analyzed by chong (07062020)
+dat_wt_manual <- read.xlsx("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_ctrl_07042020_manualy.xlsx") %>% 
+  mutate('Rearing'= Total_rearing/Total_frame*600, 'First_rearing'=First_rearing*0.1, 
+         'Guarding'= (Guarding - Frame_1st_crossing)*0.1, 'Acceleration'= (Acceleration -Frame_1st_crossing )*0.1) %>% 
+  pivot_longer(-c(Group, ID), names_to = "variable", values_to = 'value') %>% 
+  mutate(Group=factor(Group, levels=c("Ctrl", "Con")))
+
+p_licking <- dat_wt_manual %>% 
+  filter(variable=="First_licking") %>% 
+  ggplot(., aes(Group, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency to 1st licking (s)")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  scale_y_continuous(limits = c(0, 50), expand = c(0,0))+
+  theme(legend.position = "none")+
+  annotate(x=c(1,1,2,2), y=c(40,42,42,40),"path")+
+  annotate("text",x=1.5,y=42, label="**", size=5)
+
+p_licking_test <- dat_wt_manual %>% 
+  filter(variable=="First_licking") %>% 
+  wilcox.test(value~Group, .)
+
+p_1st_rearing <- dat_wt_manual %>% 
+  filter(variable=="First_rearing") %>% 
+  ggplot(., aes(Group, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency to first rearing (s)")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  theme(legend.position = "none")+
+  annotate(x=c(1,1,2,2), y=c(60,62,62,60),"path")+
+  annotate("text",x=1.5,y=62, label="*", size=5)
+
+p_1st_rearing_test <- dat_wt_manual %>% 
+  filter(variable=="First_rearing") %>% 
+  wilcox.test(value~Group,.)
+
+p_rearing <- dat_wt_manual %>% 
+  filter(variable=="Rearing") %>% 
+  ggplot(., aes(Group, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="# rearing per min ")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  scale_y_continuous(limits = c(1, 10))+
+  theme(legend.position = "none")+
+  annotate(x=c(1,1,2,2), y=c(9,9.1,9.1,9),"path")+
+  annotate("text",x=1.5,y=9.1, label="*", size=5)
+
+p_rearing_test<- dat_wt_manual %>% 
+  filter(variable=="Rearing") %>% 
+  wilcox.test(value~Group,.)
+
+p_Guarding <- dat_wt_manual %>% 
+  filter(variable=="Guarding") %>% 
+  ggplot(., aes(Group, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency to 1st guarding (s) ")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  scale_y_continuous(limits = c(2, 25))+
+  theme(legend.position = "none")+
+  annotate(x=c(1,1,2,2), y=c(23,24,24,23),"path")+
+  annotate("text",x=1.5,y=24, label="*", size=5)
+
+p_Guarding_test<- dat_wt_manual %>% 
+  filter(variable=="Guarding") %>% 
+  wilcox.test(value~Group,.)
+
+p_Acceleration <- dat_wt_manual %>% 
+  filter(variable=="Acceleration") %>% 
+  ggplot(., aes(Group, value, fill=Group))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitterdodge(), shape=1)+
+  scale_fill_manual(values=c("seagreen", "indianred"))+
+  labs(x="", y="Latency to 1st acceleration (s) ")+
+  theme(axis.line.x = element_line(),
+        axis.line.y = element_line(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title=element_text(family = "Arial",size = 12, face ="plain"))+
+  theme(legend.position = "none")+
+  annotate(x=c(1,1,2,2), y=c(75,77,77,75),"path")+
+  annotate("text",x=1.5,y=77, label="***", size=5)
+
+p_Acceleration_test<- dat_wt_manual %>% 
+  filter(variable=="Acceleration") %>% 
+  wilcox.test(value~Group,.)
+p_pain_compare2 <- plot_grid(p_licking, p_1st_rearing, p_rearing, p_Acceleration, nrow = 1)
+p_con <- plot_grid(p_pain_compare1, p_pain_compare2, nrow = 2, rel_heights = c(2, 1))
+
+setwd("~cchen2/Documents/neuroscience/Pn\ project/Figure/PDF/")
+cairo_pdf("p_con.pdf", width = 120/25.6, height = 180/25.6, family = "Arial")
+p_con
+dev.off()
+## heat map plot to show the path----
+
+cc_behavior_reshape <- function(path_ID, frame_rate, length_pix) {
+  dat_anti <- read.csv(path_ID, header = F, stringsAsFactors = F)
+  
+  dat_anti <- dat_anti[-c(1:3), ]
+  ## only slect the position of head
+  dat_anti <- as.data.frame(apply(dat_anti, 2, as.numeric))
+  
+  ## normalize the data, zero point value from fiji
+  dat_anti$V2<- dat_anti$V2 - dat_anti$V8
+  dat_anti$V3<- dat_anti$V3 - dat_anti$V9
+  dat_anti$V5 <- dat_anti$V5 - dat_anti$V8
+  dat_anti$V6 <- dat_anti$V6 - dat_anti$V9
+  dat_anti <- dat_anti[,1:6]
+  
+  colnames(dat_anti)<- c("Frame", "Head_x","Head_y", "Prob", "Tail_x", "Tail_y")
+  
+  ## remove the frame for the first few frame
+  dat_anti <- dat_anti[seq(1, nrow(dat_anti), frame_rate/10), ]
+  dat_anti <- subset(dat_anti, Prob >0.5 | Frame> 60)
+  
+  r_pix_mm <- 165 / length_pix
+  dat_anti$Head_x <- abs(dat_anti$Head_x * r_pix_mm)
+  dat_anti$Head_y <- abs(dat_anti$Head_y * r_pix_mm)
+  
+  dat_anti$Tail_x <- abs(dat_anti$Tail_x * r_pix_mm)
+  dat_anti$Tail_y <- abs(dat_anti$Tail_y * r_pix_mm)
+  
+  chamber_div <- 165.5 ## the lenght of two chambels and the gap between
+  
+  ## assign each frame in hot as 1 (48) and nor plate(30) as 2
+  dat_anti$chamber <- ifelse(dat_anti$Head_y < chamber_div, "Hot","Nor")
+  return(dat_anti)
+ 
+}
+
+## for m1 day1
+path_ctrl <- list(str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_ctrl/T",1),
+                  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_ctrl/T",5),
+                  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_ctrl/T",7))
+cc_plot <-function(x){
+  p_heat <- ggplot(x, aes(x=Head_y, y=Head_x) ) +
+    geom_hex() +
+    scale_fill_viridis()+
+    theme_void()
+  
+  return(p_heat)
+}
+  
+dat_heat_ctrl <- path_ctrl %>% 
+  mapply(function(x) list.files(x, pattern = ".csv", full.names = T),., SIMPLIFY = F) %>% 
+  mapply(function(x) x[1], ., SIMPLIFY = F) %>% 
+  mapply(cc_behavior_reshape, ., frame_rate = 10, length_pix=364, SIMPLIFY = F) %>% 
+  mapply(cc_plot,., SIMPLIFY = F) 
+
+p_heat_ctrl <- plot_grid(dat_heat_ctrl[[1]], dat_heat_ctrl[[2]], dat_heat_ctrl[[3]], ncol = 1)
+  
+setwd("~cchen2/Documents/neuroscience/Pn\ project/Figure/PDF/")
+cairo_pdf("p_heat_ctrl.pdf", width = 90/25.6, height = 120/25.6, family = "Arial")
+p_heat_ctrl
+dev.off()
+
+path_con <- list(str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_con/T",1),
+                  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_con/T",5),
+                  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_con/T",7))
+
+
+dat_heat_con <- path_con %>% 
+  mapply(function(x) list.files(x, pattern = ".csv", full.names = T),., SIMPLIFY = F) %>% 
+  mapply(function(x) x[1], ., SIMPLIFY = F) %>% 
+  mapply(cc_behavior_reshape, ., frame_rate = 10, length_pix=364, SIMPLIFY = F) %>% 
+  mapply(cc_plot,., SIMPLIFY = F) 
+
+p_heat_con <- plot_grid(dat_heat_con[[1]], dat_heat_con[[2]], dat_heat_con[[3]], ncol = 1)
+
+setwd("~cchen2/Documents/neuroscience/Pn\ project/Figure/PDF/")
+cairo_pdf("p_heat_con.pdf", width = 90/25.6, height = 120/25.6, family = "Arial")
+p_heat_con
+dev.off()
+
+path_con <-  str_c("~cchen2/Documents/neuroscience/Pn\ project/Data_analysis/miniscope/wt_ctrl_07042020/wt_con/T",i)
+
